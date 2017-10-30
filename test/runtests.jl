3d74d6fce5221da83850790bb7702ddda446de57
Mike J Innes <mike.j.innes@gmail.com>
2017-10-30 13:37:06 +0000

    overall testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,9 +1,14 @@
 using CUSOLVER
 using Base.Test
 
+@testset "CUSOLVER" begin
+
 @testset "dense" begin
     include("dense.jl")
 end
+
 @testset "sparse" begin
     include("sparse.jl")
+end
+
 end
